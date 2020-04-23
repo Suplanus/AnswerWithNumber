e9b3cd56b86a93fa6c2c18fc27a5c3f5c27a2c32
Suplanus <info@suplanus.de>
2020-04-23 12:51:00 +0200

    Build with variables

---
+++ build/appcenter_post_build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "---"
+echo "Start post build script..."
+echo "Arguments for updating:"
+echo " - AppCenterAndroidSecret: $APPCENTER_ANDROID_SECRET"
+echo " - AppCenterIosSecret: $APPCENTER_IOS_SECRET"
+echo "---"
+
+# Replace
+IdFile=$BUILD_REPOSITORY_LOCALPATH/src/AnswerWithNumber/AnswerWithNumber/Model/AppSecretsCloud.cs
+sed -i '' "s/AppCenterAndroidSecret/$APPCENTER_ANDROID_SECRET/g" $IdFile
+sed -i '' "s/AppCenterIosSecret/$APPCENTER_IOS_SECRET/g" $IdFile
+
+# Print out file for reference
+echo "---"
+cat $IdFile
+echo "---"
+echo "End post build script..."
