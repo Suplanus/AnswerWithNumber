c11487cadc4d9fe4d04a95f96a7e879b40fad388
Suplanus <info@suplanus.de>
2020-05-06 09:56:07 +0200

    PanCake animations

--- src/AnswerWithNumber/AnswerWithNumber/View/MainPage.xaml.cs
+++ src/AnswerWithNumber/AnswerWithNumber/View/MainPage.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace AnswerWithNumber.View
+﻿using System.Linq;
+using AnswerWithNumber.Extension;
+using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
+
+namespace AnswerWithNumber.View
 {
   public partial class MainPage
   {
     public MainPage()
     {
       InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+      base.OnAppearing();
+
+      Color color1 = PancakeView.BackgroundGradientStops.First().Color;
+      Color color2 = PancakeView.BackgroundGradientStops.Last().Color;
+
+      while (true)
+      {
+        await PancakeView.ColorTo(color1, color2, c => PancakeView.BackgroundGradientStops.First().Color = c, 5000, Easing.Linear);
+        await PancakeView.ColorTo(color2, color1, c => PancakeView.BackgroundGradientStops.Last().Color = c, 5000, Easing.Linear);  
+        await PancakeView.ColorTo(color1, color2, c => PancakeView.BackgroundGradientStops.First().Color = c, 5000, Easing.Linear);
+        await PancakeView.ColorTo(color2, color1, c => PancakeView.BackgroundGradientStops.Last().Color = c, 5000, Easing.Linear);  
+      }
     }
   }
 }
